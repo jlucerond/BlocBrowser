1b03fcec9adff6773f75a49bcb8b8b3fa20db366
Joe Lucero <jlucerond@gmail.com>
2015-05-06 16:16:11 -0400

    Checkpoint 24 Complete
    Added all of the code necessary to make a custom
    JLAwesomeFloatingToolbar UIView. Additionally, I used #define to
    refactor some NSLocalizedStrings, allowing it to more easily be
    converted to other languages. Finally, I figured out how to make the
    custom toolbar go to the center/bottom of the screen, no matter the
    orientation.

---
+++ BlocBrowser/JLAwesomeFloatingToolbar.h
@@ -0,0 +1,35 @@
+//
+//  JLAwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Joe Lucero on 5/6/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class  JLAwesomeFloatingToolbar;
+
+@protocol JLAwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+// why is this one placed here above the @interface? is this because the methods here are optional while the ones below (after @interface) are required?
+
+- (void) floatingToolbar: (JLAwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle: (NSString *)title;
+
+@end
+
+// interface for toolbar?
+@interface JLAwesomeFloatingToolbar : UIView
+
+// must initialize with an array of titles
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+// the button is enabled or not based on the title?
+- (void) setEnabled:(BOOL) enabled forButtonWithTitle:(NSString *)title;
+
+// not sure what delegate means here
+@property (nonatomic, weak) id <JLAwesomeFloatingToolbarDelegate> delegate;
+
+@end
